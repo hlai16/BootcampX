e32e4db66e68be4351abff692fa373204ca393ca
hlai16 <gloriajoanlai@gmail.com>
2021-10-12 18:39:42 -0400

    4_queryies query 5 completion

--- 4_queries/6_cohort_with_longest_assistances.sql
+++ 4_queries/6_cohort_with_longest_assistances.sql
@@ -3,4 +3,6 @@ FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-HAVING MAX(average_assistance_request_time) = average_assistance_request_time;
+ORDER BY average_assistance_request_time DESC
+LIMIT 1;
+
