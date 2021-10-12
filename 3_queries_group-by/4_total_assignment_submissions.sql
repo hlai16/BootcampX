6ec287169630185c53459b710aa8d8d3e196e7e2
hlai16 <gloriajoanlai@gmail.com>
2021-10-12 00:25:29 -0400

    Attempting GROUP BY query 4, but forgetting the syntax of MAX/MIN.

---
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort, COUNT(assignment_submissions.*) AS total_submissions
+FROM cohorts JOIN students
+ON cohorts.id = cohort_id
+JOIN assignment_submissions
+ON students.id = student_id
+GROUP BY cohort
+ORDER BY MAX(assignment_submissions.*);
