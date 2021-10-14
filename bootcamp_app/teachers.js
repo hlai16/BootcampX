fcef54c82ee06ca60d4b3be9001cbf792fe5a327
hlai16 <gloriajoanlai@gmail.com>
2021-10-13 23:34:51 -0400

    SQL injection challenge completed

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teacher, cohort
 ORDER BY teacher;
-`)
+`
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`];
+
+pool.query(queryString, values)
 .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
     })
   })
-  
+
 .catch(err => console.error('query error', err.stack));
